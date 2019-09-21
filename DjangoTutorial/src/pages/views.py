bab0e13cfadaca100fd93286d5a7bba6f28c06f7
ElijahCoding <cuiyeqing960904@gmail.com>
2019-09-21 17:07:04 +0300

    building templates

--- DjangoTutorial/src/pages/views.py
+++ DjangoTutorial/src/pages/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def home_view(request, *args, **kwargs):
+    return render(request, "home.html", {})
+
+def contact_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Contact</h1>")
+
+def about_view(request, *args, **kwargs):
+    return HttpResponse("<h1>About</h1>")
+
+def social_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Social</h1>")
--- DjangoTutorial/src/trydjango/settings.py
+++ DjangoTutorial/src/trydjango/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'products',
-
+    'pages',
+    'products'
 ]
 
 MIDDLEWARE = [
