8cce98548f7708917cc637acfd2e8daab3e59bb3
ElijahCoding <cuiyeqing960904@gmail.com>
2019-09-23 22:46:35 +0300

    building blog

--- Django2.2/src/try_django/views.py
+++ Django2.2/src/try_django/views.py
@@ -1,14 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.loader import get_template
 
 def home_page(request):
     return render(request, "home.html", {})
 
 def about_page(request):
-    return HttpResponse('<h1>about</h1>')
+    return render(request, "about.html", {"title": "About"})
 
 def contact_page(request):
     return HttpResponse('<h1>contact</h1>')
 
 def example_page(request):
-    return HttpResponse('<h1>example</h1>')
+    context = {"title": "Example"}
+    template_name = "hello_world.html"
+    template_obj = get_template(template_name)
+    rendered_item = template_obj.render(context)
+    return HttpResponse(rendered_item)
