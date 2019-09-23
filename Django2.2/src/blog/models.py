e9f0cef3d26c06ed3f171fff3b4f64f664a0b023
ElijahCoding <cuiyeqing960904@gmail.com>
2019-09-23 23:01:46 +0300

    model built

--- Django2.2/src/blog/models.py
+++ Django2.2/src/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=120)
+    slug = models.SlugField(unique=True)
+    content = models.TextField(null=True, blank=True)
+    publish_date = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
