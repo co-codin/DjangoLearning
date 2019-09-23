13d79a225e85dd557daabea92dd862c084acc344
ElijahCoding <cuiyeqing960904@gmail.com>
2019-09-23 23:17:59 +0300

    preparing db

--- Django2.2/src/blog/views.py
+++ Django2.2/src/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 from .models import BlogPost
 
+
+def blog_post_detail_view(request):
+    obj = BlogPost.objects.get(id=1)
+    template_name = 'blog_post_detail.html'
+    context = { "object": obj }
+    return render(request, template_name, context)
--- Django2.2/src/try_django/urls.py
+++ Django2.2/src/try_django/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, re_path
 
+from blog.views import (
+    blog_post_detail_view
+)
+
 from .views import (
     home_page,
     about_page,
@@ -12,6 +16,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', home_page),
+    path('blog/', blog_post_detail_view),
     re_path(r'^pages?/$', about_page),
     re_path(r'^about/$', about_page),
     path('example/', example_page),
