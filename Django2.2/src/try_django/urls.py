33ef0e24a780e69fe58aa2d3f494855239f8ddee
ElijahCoding <cuiyeqing960904@gmail.com>
2019-09-24 12:15:56 +0300

    refactored to blog urls

--- Django2.2/src/try_django/urls.py
+++ Django2.2/src/try_django/urls.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 
 from blog.views import (
     blog_post_create_view,
-    blog_post_detail_view,
-    blog_post_list_view,
-    blog_post_update_view,
-    blog_post_delete_view
 )
 
 from .views import (
@@ -20,13 +16,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_page),
 
-    path('blog/', blog_post_list_view),
-
-    path('blog/<str:slug>/', blog_post_detail_view),
-    path('blog/<str:slug>/edit/', blog_post_edit_view),
-    path('blog/<str:slug>/delete/', blog_post_delete_view),
-
     path('blog-new/', blog_post_create_view),
+    path('blog/', include('blog.urls')),
 
     re_path(r'^pages?/$', about_page),
     re_path(r'^about/$', about_page),
