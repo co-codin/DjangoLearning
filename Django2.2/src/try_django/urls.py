e64824d463883b0dd88be4c8512cdd7abe9ef67d
ElijahCoding <cuiyeqing960904@gmail.com>
2019-09-24 12:08:02 +0300

    include urls. refactor blog funcs to blog pluggable app

--- Django2.2/src/try_django/urls.py
+++ Django2.2/src/try_django/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import path, re_path
 
 from blog.views import (
+    blog_post_create_view,
     blog_post_detail_view,
-    blog_post_list_view
+    blog_post_list_view,
+    blog_post_update_view,
+    blog_post_delete_view
 )
 
 from .views import (
@@ -18,7 +21,13 @@ urlpatterns = [
     path('', home_page),
 
     path('blog/', blog_post_list_view),
+
     path('blog/<str:slug>/', blog_post_detail_view),
+    path('blog/<str:slug>/edit/', blog_post_edit_view),
+    path('blog/<str:slug>/delete/', blog_post_delete_view),
+
+    path('blog-new/', blog_post_create_view),
+
     re_path(r'^pages?/$', about_page),
     re_path(r'^about/$', about_page),
     path('example/', example_page),
