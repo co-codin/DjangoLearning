80fcca81bde164746ff6eeaca10e35cf9586c452
ElijahCoding <cuiyeqing960904@gmail.com>
2019-09-24 08:56:09 +0300

    handling dynamic errors

--- Django2.2/src/blog/views.py
+++ Django2.2/src/blog/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from .models import BlogPost
 
 
-def blog_post_detail_view(request):
-    obj = BlogPost.objects.get(id=1)
+def blog_post_detail_view(request, post_id):
+    obj = BlogPost.objects.get(id=post_id)
     template_name = 'blog_post_detail.html'
     context = { "object": obj }
     return render(request, template_name, context)
