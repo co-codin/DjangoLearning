142ed634e826554072cf31188b2a50a2f86abeea
ElijahCoding <cuiyeqing960904@gmail.com>
2019-09-24 09:30:47 +0300

    blog CRUD structure built

--- Django2.2/src/blog/views.py
+++ Django2.2/src/blog/views.py
@@ -1,16 +1,37 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import BlogPost
 
+def blog_post_list_view(request):
+    # list out objects
+    # could be search
+    template_name = 'blog_post_list.html'
+    context = { 'object_list': [] }
+    return render(request, template_name, context)
 
-def blog_post_detail_view(request, post_id):
-    try:
-        obj = BlogPost.objects.get(id=post_id)
-    except BlogPost.DoesNotExist:
-        raise Http404
-    except ValueError:
-        raise Http404
+def blog_post_create_view(request):
+    # create objects
+    # ? use a form
+    # request.user -> return something
+    template_name = 'blog_post_create.html'
+    context = { 'form': None }
+    return render(request, template_name, context)
 
+def blog_post_detail_view(request, slug):
+    # 1 object -> detail view
+    obj = get_object_or_404(BlogPost, slug=slug)
     template_name = 'blog_post_detail.html'
-    context = { "object": obj }
+    context = { 'object': obj }
+    return render(request, template_name, context)
+
+def blog_post_update_view(request, slug):
+    obj = get_object_or_404(BlogPost, slug=slug)
+    template_name = 'blog_post_update.html'
+    context = {'object': obj, 'form': None}
+    return render(request, template_name, context)
+
+def blog_post_delete_view(request, slug):
+    obj = get_object_or_404(BlogPost, slug=slug)
+    template_name = 'blog_post_delete.html'
+    context = {'object': obj}
     return render(request, template_name, context)
