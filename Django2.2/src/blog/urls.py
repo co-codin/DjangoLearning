f1f48f0f2583074f02705ae5c8280cc3df0b2c57
ElijahCoding <cuiyeqing960904@gmail.com>
2019-09-26 17:41:25 +0300

    upload images

--- Django2.2/src/blog/urls.py
+++ Django2.2/src/blog/urls.py
@@ -5,6 +5,7 @@ from .views import (
     blog_post_update_view,
     blog_post_delete_view,
 )
+from django.conf import settings
 
 urlpatterns = [
     path('', blog_post_list_view),
@@ -12,3 +13,8 @@ urlpatterns = [
     path('<str:slug>/edit/', blog_post_update_view),
     path('<str:slug>/delete/', blog_post_delete_view),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
