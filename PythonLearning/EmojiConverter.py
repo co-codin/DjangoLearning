9e429a9d4ed83bf91ceedcc24f4392f5bd185985
ElijahCoding <cuiyeqing960904@gmail.com>
2019-09-27 17:25:54 +0300

    code to func

--- PythonLearning/EmojiConverter.py
+++ PythonLearning/EmojiConverter.py
@@ -1,13 +1,14 @@
+def emoji_converter(message):
+    words = message.split(' ')
+    emojis = {
+        ":)": "😀",
+        ":(": "😭"
+    }
+    output = ''
+    for word in words:
+        output += emojis.get(word, word) + ' '
+    return output
+
 message = input(">")
-words = message.split(' ')
-
-emojis = {
-    ":)": "😀",
-    ":(": "😭"
-}
-
-output =  ''
-for word in words:
-    output += emojis.get(word, word) + ' '
-
-print(output)
+result = emoji_converter(message)
+print(result)
