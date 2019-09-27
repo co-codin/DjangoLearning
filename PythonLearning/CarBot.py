2bf164bb686a3d79772af633349c432b35f76641
ElijahCoding <cuiyeqing960904@gmail.com>
2019-09-27 14:34:09 +0300

    some new features

--- PythonLearning/CarBot.py
+++ PythonLearning/CarBot.py
@@ -1,13 +1,26 @@
 command = ''
-while command.lower() != 'quit':
+started = False
+
+while True:
     command = input('> ').lower()
     if command == 'start':
-        print('Car Started..')
+        if started:
+            print('Car is already started.')
+        else:
+            started = True
+            print('Car Started..')
     elif command == 'stop':
-        print('Car stopped.')
+        if not started:
+            print('Car is already stopped')
+        else:
+            print('Car stopped.')
     elif command == 'help':
         print(""""
             start - to start the car
             stop - to stop the car
             quit - to quit
         """)
+    elif command == 'quit':
+        break
+    else:
+        print("I don't understand you")
