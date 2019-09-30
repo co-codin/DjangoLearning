8f6cedafc18815948b0618a3bb5835bf5734c310
ElijahCoding <cuiyeqing960904@gmail.com>
2019-09-30 15:51:59 +0300

    products list

--- RestAPI/FirstApiDjango/onlinestore/products/views.py
+++ RestAPI/FirstApiDjango/onlinestore/products/views.py
@@ -1,2 +1,11 @@
+from django.http import JsonResponse
+
 from .models import Product, Manufacturer
 
+def product_list(request):
+    products = Product.objects.all()
+    data = {
+        "products": list(products.values())
+    }
+    response = JsonResponse(data)
+    return response
