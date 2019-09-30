47c660500b28acc9473d415711c23b5bd6b96bed
ElijahCoding <cuiyeqing960904@gmail.com>
2019-09-30 23:17:38 +0300

    building news app with rest framework

--- RestApiFramework/newsapi/newsapi/settings.py
+++ RestApiFramework/newsapi/newsapi/settings.py
@@ -47,6 +47,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'rest_framework',
+    'news'
 ]
 
 ROOT_URLCONF = 'newsapi.urls'
