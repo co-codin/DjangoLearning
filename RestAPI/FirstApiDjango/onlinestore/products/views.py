298552316e8853385b50e9b4a9e5557cbb91c1bc
ElijahCoding <cuiyeqing960904@gmail.com>
2019-09-30 15:55:18 +0300

    detail product

--- RestAPI/FirstApiDjango/onlinestore/products/views.py
+++ RestAPI/FirstApiDjango/onlinestore/products/views.py
@@ -9,3 +9,29 @@ def product_list(request):
     }
     response = JsonResponse(data)
     return response
+
+def product_detail(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+        data = {
+            "product": {
+                "name": product.name,
+                "manufacturer": product.manufacturer.name,
+                "description": product.description,
+                "photo": product.photo.url,
+                "price": product.price,
+                "shipping_cost": product.shipping_cost,
+                "quantity": product.quantity
+            }
+        }
+        response = JsonResponse(data)
+    except Product.DoesNotExist:
+        response = JsonResponse({
+            "error": {
+                "code": 404,
+                "message": "product not found!"
+            }},
+            status=404
+        )
+
+    return response
