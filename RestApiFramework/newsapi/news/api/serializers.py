8dd3409619f51ff718302cc686ad9f39d3a9f6e6
ElijahCoding <cuiyeqing960904@gmail.com>
2019-10-01 18:20:58 +0300

    building serializers

---
+++ RestApiFramework/newsapi/news/api/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+class ArticleSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    # author = serializers.CharField()
+    title = serializers.CharField()
+    description = serializers.CharField()
+    body = serializers.CharField()
+    location = serializers.CharField()
+    publication_date = serializers.DateField()
+    active = serializers.BooleanField()
+    created_at = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
