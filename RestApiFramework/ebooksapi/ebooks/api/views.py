6b66f5be63eb3e1175114458491115ee5d52b4e4
ElijahCoding <cuiyeqing960904@gmail.com>
2019-10-03 19:59:42 +0300

    serializers built

--- RestApiFramework/ebooksapi/ebooks/api/views.py
+++ RestApiFramework/ebooksapi/ebooks/api/views.py
@@ -6,3 +6,8 @@ class ReviewSerializer(serializers.ModelSerializer):
         model = Review
         fields = "__all__"
 
+class EbookSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True, read_only=True)
+    class Meta:
+        model = Ebook
+        fields = "__all__"
