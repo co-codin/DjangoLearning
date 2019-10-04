99c7e9ba0ca37551f315ccff891e83477b03adf9
ElijahCoding <cuiyeqing960904@gmail.com>
2019-10-04 17:11:50 +0300

    reviews by users

--- RestApiFramework/ebooksapi/ebooks/api/views.py
+++ RestApiFramework/ebooksapi/ebooks/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from rest_framework.generics import get_object_or_404
 from rest_framework import permissions
+from rest_framework.exceptions import ValidationError
 from ebooks.api.permissions import IsAdminUserOrReadOnly
 
 from ebooks.models import Ebook, Review
@@ -19,11 +20,20 @@ class EbookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
 class ReviewCreateAPIView(generics.ListCreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         ebook_pk = self.kwargs.get('ebook_pk')
         ebook = get_object_or_404(Ebook, pk=ebook_pk)
-        serializer.save(ebook=ebook)
+
+        review_author = self.request.user
+        review_queryset = Review.objects.filter(ebook=ebook,
+                                                review_author=review_author
+                                                )
+        if review_queryset.exists():
+            raise ValidationError("You have already reviewed")
+
+        serializer.save(ebook=ebook, review_author=review_author)
 
 class ReviewDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     Review = Ebook.objects.all()
