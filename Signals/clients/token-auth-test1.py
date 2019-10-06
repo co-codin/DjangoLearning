61e0ffcedefa3150f875c7f65bd0ab39356bd700
ElijahCoding <cuiyeqing960904@gmail.com>
2019-10-06 20:57:22 +0300

    client requests to test api token

---
+++ Signals/clients/token-auth-test1.py
@@ -0,0 +1,11 @@
+import requests
+
+def client():
+    credentials = {"username": "elijah", "password": "hellojava"}
+    response = requests.post("http://127.0.0.1:8000/api/rest-auth/login/", data=credentials)
+    print("Status Code: ", response.status_code)
+    response_data = response.json()
+    print(response_data)
+
+if __name__ == "__main__":
+    client()
