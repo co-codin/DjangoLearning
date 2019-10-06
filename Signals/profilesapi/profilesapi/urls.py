46422e0fa7bd846f2b0243b098e5d07a348edbd8
ElijahCoding <cuiyeqing960904@gmail.com>
2019-10-06 23:03:18 +0300

    fetching data with token

--- Signals/profilesapi/profilesapi/urls.py
+++ Signals/profilesapi/profilesapi/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('profiles.api.urls')),
     path("api-auth/", include("rest_framework.urls")),
     path("api/rest-auth/", include("rest_auth.urls"))
 ]
