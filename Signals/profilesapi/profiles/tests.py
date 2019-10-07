697446bef21be9cbf6d4ebc5c803dfd313aae1b9
ElijahCoding <cuiyeqing960904@gmail.com>
2019-10-07 14:40:15 +0300

    profile list test

--- Signals/profilesapi/profiles/tests.py
+++ Signals/profilesapi/profiles/tests.py
@@ -17,3 +17,24 @@ class RegistrationTestCase(APITestCase):
                 "password2": "hellojava"}
         response = self.client.post("/api/rest-auth/registration/", data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+class ProfileViewSetTestCase(APITestCase):
+    list_url = reverse("profile-list")
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="davinci",
+                                             password="some-very-strong-psw")
+        self.token = Token.objects.create(user=self.user)
+        self.api_authentication()
+
+    def api_authentication(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token.key)
+
+    def test_profile_list_authenticated(self):
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_profile_list_unauthenticated(self):
+        self.client.force_authenticate(user=None)
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
