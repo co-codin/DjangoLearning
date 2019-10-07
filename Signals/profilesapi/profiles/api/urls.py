02a2ea63467539a8c3e5532eab065739c4475c99
ElijahCoding <cuiyeqing960904@gmail.com>
2019-10-07 10:34:17 +0300

    viewset

--- Signals/profilesapi/profiles/api/urls.py
+++ Signals/profilesapi/profiles/api/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path
-from profiles.api.views import ProfileList
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from profiles.api.views import ProfileViewSet
+
+# profile_list = ProfileViewSet.as_view({"get": "list"})
+# profile_detail = ProfileViewSet.as_view({"get": "retrieve"})
+
+router = DefaultRouter()
+router.register(r"profiles", ProfileViewSet)
 
 urlpatterns = [
-    path('profiles/', ProfileList.as_view(), name='profile-list')
+    path("", include(router.urls))
+    # path('profiles/', profile_list, name='profile-list'),
+    # path('profiles/<int:pk>/', profile_detail, name='profile-detail')
 ]
