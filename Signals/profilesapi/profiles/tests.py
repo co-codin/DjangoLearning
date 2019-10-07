d89d4f9365098cf1a22965a5e579e15fcc2b69dd
ElijahCoding <cuiyeqing960904@gmail.com>
2019-10-07 14:27:36 +0300

    first test, registration test

--- Signals/profilesapi/profiles/tests.py
+++ Signals/profilesapi/profiles/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+
+from profiles.api.serializers import ProfileSerializer, ProfileStatusSerializer
+from profiles.models import Profile, ProfileStatus
+
+class RegistrationTestCase(APITestCase):
+    def test_registration(self):
+        data = {"username": "testcase",
+                "email": "testcase@test.com",
+                "password1": "hellojava",
+                "password2": "hellojava"}
+        response = self.client.post("/api/rest-auth/registration/", data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
