2e2bd49517950ca68ed7dda736bd47db108e57ca
ElijahCoding <cuiyeqing960904@gmail.com>
2019-11-20 08:59:33 +0300

    show, update, destroy

--- RestFramework/cars/urls.py
+++ RestFramework/cars/urls.py
@@ -5,6 +5,7 @@ from .views import *
 app_name = 'car'
 
 urlpatterns = [
+    path('car/create', CarCreateView.as_view()),
     path('all/', CarsListView.as_view()),
-    path('car/create', CarCreateView.as_view())
+    path('car/detail/<int:pk>/', CarDetailView.as_view())
 ]
--- RestFramework/cars/views.py
+++ RestFramework/cars/views.py
@@ -9,3 +9,8 @@ class CarCreateView(generics.CreateAPIView):
 class CarsListView(generics.ListAPIView):
     serializer_class = CarsListSerializer
     queryset = Car.objects.all()
+
+
+class CarDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = CarDetailSerializer
+    queryset = Car.objects.all()
