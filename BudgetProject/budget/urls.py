33440a101374a99ac2eefdd8d339b56f1120b103
ElijahCoding <cuiyeqing960904@gmail.com>
2019-12-14 12:21:32 +0300

    basic templates built

--- BudgetProject/budget/urls.py
+++ BudgetProject/budget/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import project_list
+from .views import project_list, project_detail
 
 app_name='budget'
 
 urlpatterns = [
-    path('', project_list, name='list')
+    path('', project_list, name='list'),
+    path('<slug:project_slug>', project_detail, name='detail')
 ]
