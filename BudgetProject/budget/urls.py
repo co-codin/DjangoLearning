f5e9738eeca41e821f0f8c930758dc2d9140e05a
ElijahCoding <cuiyeqing960904@gmail.com>
2019-12-16 09:08:01 +0300

    list and detail

--- BudgetProject/budget/urls.py
+++ BudgetProject/budget/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from .views import project_list, project_detail
 
-app_name='budget'
-
 urlpatterns = [
     path('', project_list, name='list'),
     path('<slug:project_slug>', project_detail, name='detail')
--- BudgetProject/budget/views.py
+++ BudgetProject/budget/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Project
 
 def project_list(request):
-    return render(request, 'budget/project-list.html')
+    project_list = Project.objects.all()
+    return render(request, 'budget/project-list.html', { 'project_list': project_list })
 
 def project_detail(request, project_slug):
-    return render(request, 'budget/project-detail.html')
+    project = get_object_or_404(Project, slug=project_slug)
+
+    return render(request, 'budget/project-detail.html', {
+        'project': project,
+        'expense_list': project.expenses.all()
+    })
